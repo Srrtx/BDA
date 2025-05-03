eff394278d582bf1f7eaac4f920ec0217c5529a2
Srrt <113283637+Srrtx@users.noreply.github.com>
2025-05-03 13:40:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import make_blobs
 
-#Set the page config
+#Ste the page config
 st.set_page_config(page_title="K-Means Clustering", layout="centered")
 
 #Load model 
@@ -12,46 +11,19 @@ with open('kmeans_model.pkl','rb') as f:
     loaded_model = pickle.load(f)
     
 #Set title 
-st.title("K-Means Clustering Visualizer Customer Segmentation")
+st.title("K-Means Clustering Visualizer ")
 
 #Load Dataset
-df = pd.read_csv('marketing_campaign.csv')
-
-
-# Define feature names (make sure these match exactly with DataFrame columns)
-#feature_names = ['Year_Birth', 'Education', 'Marital_Status', 'Income', 'Kidhome', 
-#               'Teenhome', 'Recency', 'MntWines', 'MntFruits', 'MntMeatProducts',
-#                'MntFishProducts', 'MntSweetProducts', 'MntGoldProds', 'NumDealsPurchases',
-#                'NumWebPurchases', 'NumCatalogPurchases', 'NumStorePurchases', 
-#                'NumWebVisitsMonth', 'AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5',
-#                'AcceptedCmp1', 'AcceptedCmp2', 'Complain', 'Response']
-
-# Verify columns exist in DataFrame
-# missing_cols = [col for col in feature_names if col not in df.columns]
-# if missing_cols:
-#     st.error(f"Missing columns in dataset: {missing_cols}")
-#     st.stop()
-
-# Get the feature columns for clustering
-# X = df[feature_names].values
-
-# Add feature selection dropdowns
-# x_axis = st.selectbox('Select X-axis feature', feature_names)
-# y_axis = st.selectbox('Select Y-axis feature', feature_names)
-
-# # Get feature indices
-# x_idx = feature_names.index(x_axis)
-# y_idx = feature_names.index(y_axis)
+X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
 
 #Predict using the loaded model
-# y_kmeans = loaded_model.predict(X)
+y_kmeans = loaded_model.predict(X)
 
 #plotting
 fig, ax = plt.subplots()
-scatter = ax.scatter(X[:, x_idx], X[:, y_idx], c=y_kmeans, cmap='viridis')
-ax.scatter(loaded_model.cluster_centers_[:, x_idx], loaded_model.cluster_centers_[:, y_idx], s=300, c='red', label='Centroids')
+scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red',label='Centroids')
 ax.set_title('k-Means Clustering')
-ax.set_xlabel(x_axis)
-ax.set_ylabel(y_axis)
 ax.legend()
 st.pyplot(fig)
+#Display
